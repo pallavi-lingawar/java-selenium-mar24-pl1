faaa33f955b6bb2cc3c609132f7231ab2ce814ca
pallavi-lingawar <pallavi40340@gmail.com>
2024-03-20 13:19:08 -0700

    revised steps

--- src/com/allianz/tabs/Demo1.java
+++ src/com/allianz/tabs/Demo1.java
@@ -11,7 +11,11 @@ public class Demo1 {
 	public static void main(String[] args) 
 	{
 
-
+		WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        
+        driver.get("https://db4free.net");
    
 	}
 
